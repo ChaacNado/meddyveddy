85c8afbfce5f45bad702038a58a846898a5b98ae
ChaacNado <35525138+ChaacNado@users.noreply.github.com>
2020-03-04 14:14:23 +0100

    ranged weapon object, aiming from player to mouse

---
+++ MeddyVeddy/Assets/Scripts/RangedWeaponController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedWeaponController : MonoBehaviour
+{
+    public Transform weaponHold;
+    public RangedWeapon startingWeapon;
+    RangedWeapon equippedRw;
+
+    void Start()
+    {
+        if (startingWeapon != null)
+        {
+            EquipWeapon(startingWeapon);
+        }
+    }
+
+    public void EquipWeapon(RangedWeapon rwToEquip)
+    {
+        if (equippedRw != null)
+        {
+            Destroy(equippedRw.gameObject);
+        }
+        equippedRw = Instantiate(rwToEquip, weaponHold.position, weaponHold.rotation) as RangedWeapon;
+        equippedRw.transform.parent = weaponHold;
+    }
+}
