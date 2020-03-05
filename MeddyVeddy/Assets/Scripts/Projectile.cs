9b7cd9be1cf696e212df64ee4722f522ab8dd0b3
ChaacNado <35525138+ChaacNado@users.noreply.github.com>
2020-03-05 11:48:16 +0100

    projectile collision check initialized

--- MeddyVeddy/Assets/Scripts/Projectile.cs
+++ MeddyVeddy/Assets/Scripts/Projectile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public LayerMask collisionMask;
     float speed = 10;
 
     public void SetSpeed(float newSpeed)
@@ -13,6 +14,27 @@ public class Projectile : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        float moveDistance = speed * Time.deltaTime;
+        CheckCollisions(moveDistance);
+        transform.Translate(Vector3.forward * moveDistance);
+    }
+
+    void CheckCollisions(float moveDistance)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        /// If the ray is hitting that object...
+        if (Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+            /* QueryTriggerInteraction allows us to set whether or not this will collide with trigger-colliders */
+        {
+            OnHitObject(hit);
+        }
+    }
+
+    void OnHitObject(RaycastHit hit)
+    {
+        print(hit.collider.gameObject.name);
+        Destroy(gameObject);
     }
 }
